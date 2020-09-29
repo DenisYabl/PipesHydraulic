4d74151bb3c5c33a01bed3b392594f08dc11e6d6
av-maltsev <amaltsev@isgneuro.com>
2020-09-29 19:07:27 +0300

    abstract base classes, mostly for future

---
+++ code/HE2_ABC.py
@@ -0,0 +1,21 @@
+from abc import ABC
+
+class HE2_ABC_Fluid(ABC):
+    def calc(self, P_bar, T_C):
+        pass
+
+class HE2_ABC_GraphEdge(ABC):
+    pass
+
+class HE2_ABC_GraphVertex(ABC):
+    pass
+
+class HE2_ABC_Graph(ABC):
+    pass
+
+class HE2_ABC_PipeSegment(ABC):
+    pass
+
+class HE2_ABC_Pipeline(ABC):
+    pass
+
