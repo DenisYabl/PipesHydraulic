e51a7f61151feddfe95456074816f80e022558cd
av-maltsev <amaltsev@isgneuro.com>
2020-09-29 19:08:11 +0300

    test fluid

---
+++ code/HE2_Fluid.py
@@ -0,0 +1,13 @@
+from uniflocpy.uPVT.BlackOil_model import Fluid
+from HE2_ABC import HE2_ABC_Fluid
+
+class HE2_DummyWater(HE2_ABC_Fluid):
+    def __init__(self):
+        self.rho_wat_kgm3 = 1000
+        self.mu_wat_cp = 1
+
+
+if __name__ == '__main__':
+    fl = HE2_DummyWater()
+    fl.calc(100, 100)
+    print(fl.rho_wat_kgm3)
