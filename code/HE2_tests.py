a6d668d8d87beee76eae15f21970ea6736fb6917
av-maltsev <amaltsev@isgneuro.com>
2020-10-02 09:00:15 +0300

    wider compare with Mischenko test

--- code/HE2_tests.py
+++ code/HE2_tests.py
@@ -4,6 +4,7 @@ import uniflocpy.uWell.uPipe as uPipe
 import uniflocpy.uTools.uconst as uc
 import Hydraulics.Methodics.Mukherjee_Brill as mb
 import Hydraulics.Properties.Mishenko as msch
+from itertools import product
 
 class TestWaterPipeSegment(unittest.TestCase):
     def setUp(self):
@@ -54,29 +55,37 @@ class TestWaterPipeSegment(unittest.TestCase):
     #     self.assertAlmostEqual(t, 20)
     #
     def test_5(self):
-        p0_bar = 50
-        t0_C = 20
-        x_kgs = 1
-        pipe = HE2_WaterPipeSegment()
-        pipe.inner_diam_m = 0.1
-        pipe.roughness_m = 1e-5
-        pipe.L_m = 100
-        pipe.slope_m = 0
-        p, t = pipe.perform_calc(p0_bar, t0_C, x_kgs)
-        print(uc.bar2Pa(p-p0_bar), t)
+        xs = [0.01, 0.1, 1, 10, 100]
+        ds = [0.05, 0.1, 0.25, 0.5]
+        rs = [0, 5e-6, 1e-5, 5e-5, 1e-4]
+        conditions = product(xs, ds, rs)
+        max_bias = 0
+        for x, d, r in conditions:
+            p0_bar = 50
+            t0_C = 20
+            x_kgs = x
+            pipe = HE2_WaterPipeSegment()
+            pipe.inner_diam_m = d
+            pipe.roughness_m = r
+            # pipe.L_m = 100
+            # pipe.slope_m = 0
+            p_grad = pipe.calc_P_friction_gradient_Pam(p0_bar, t0_C, x_kgs)
 
-        Q_m3day = x_kgs * 86400 / pipe.fluid.rho_wat_kgm3
-
-        oil_params = dict(CurrentP=p0_bar, PlastT=t0_C, CurrentT=t0_C, adkuLiquidDebit=Q_m3day)
-        oil_params.update(wellopVolumeWater=100, GasFactor=1e-8, PlastWaterWeight=pipe.fluid.rho_wat_kgm3/1000, wellopLiquidDensityTm=pipe.fluid.rho_wat_kgm3/1000, adkuOilDebit=0)
-        oil_params.update(VolumeOilCoeff=1.2, PlastOilDynamicViscosity=1,SepOilDynamicViscosity=1, OilSaturationP=100, PlastOilWeight=0.85, SepOilWeight=0.85, GasDensity=0.8)
-        tubing = dict(IntDiameter=pipe.inner_diam_m, angle=90, Roughness=pipe.roughness_m)
-        mishenko = msch.Mishenko.from_oil_params(oil_params, tubing)
-        misch_grad = mb.calculate(mishenko, tubing)
-        misch_dp = misch_grad * pipe.L_m
-        print(misch_dp)
-        pass
-
+            Q_m3day = x_kgs * 86400 / pipe.fluid.rho_wat_kgm3
+            oil_params = dict(CurrentP=p0_bar, PlastT=t0_C, CurrentT=t0_C, adkuLiquidDebit=Q_m3day)
+            oil_params.update(wellopVolumeWater=100, GasFactor=1e-8, PlastWaterWeight=pipe.fluid.rho_wat_kgm3/1000, wellopLiquidDensityTm=pipe.fluid.rho_wat_kgm3/1000, adkuOilDebit=0)
+            oil_params.update(VolumeOilCoeff=1.2, PlastOilDynamicViscosity=1,SepOilDynamicViscosity=1, OilSaturationP=100, PlastOilWeight=0.85, SepOilWeight=0.85, GasDensity=0.8)
+            tubing = dict(IntDiameter=pipe.inner_diam_m, angle=90, Roughness=pipe.roughness_m)
+            mishenko = msch.Mishenko.from_oil_params(oil_params, tubing)
+            misch_grad = mb.calculate(mishenko, tubing)
+            # self.assertAlmostEqual(abs(misch_grad), p_grad, 1)
+            # print(x, d, r, p_grad, misch_grad)
+            p_grad = abs(p_grad)
+            misch_grad = abs(misch_grad)
+            bias = 1 - min(p_grad, misch_grad)/max(p_grad, misch_grad)
+            if bias > max_bias:
+                max_bias = bias
+        self.assertLess(max_bias, 0.1)
 
 
 if __name__ == "__main__":
