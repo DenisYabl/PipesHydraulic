7f6935cc40c5fe4a160e1ba3e2e82764904eb593
av-maltsev <amaltsev@isgneuro.com>
2020-10-05 18:23:55 +0300

    -also need dummy edges for connect vertices to graph root

---
+++ code/HE2_SpecialEdges.py
@@ -0,0 +1,9 @@
+import HE2_ABC as abc
+
+class HE2_DummyEdge(abc.HE2_ABC_GraphEdge):
+    def __init__(self, P):
+        self.P = P
+
+    def perform_calc(self, P_bar, T_C, X_kgsec, unifloc_direction=-1):
+        return self.P, T_C
+
