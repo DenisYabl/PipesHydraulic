2c5111249c80889dd7b71d42bfcdcd8380975394
av-maltsev <amaltsev@isgneuro.com>
2020-10-05 18:22:53 +0300

    -need something to store graph vertices and boundary conditions

---
+++ code/HE2_Vertices.py
@@ -0,0 +1,14 @@
+from HE2_ABC import HE2_ABC_GraphVertex
+
+
+class HE2_Boundary_Vertex(HE2_ABC_GraphVertex):
+    def __init__(self, kind, value):
+        self.kind = kind
+        self.value = value
+
+
+class HE2_Source_Vertex(HE2_Boundary_Vertex):
+    def __init__(self, kind, value, fluid, T):
+        super().__init__(kind, value)
+        self.fluid = fluid
+        self.T = T
