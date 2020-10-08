8dc0330a0ce15021d65aaf02c423be9783f36e0b
av-maltsev <amaltsev@isgneuro.com>
2020-10-08 13:11:51 +0300

    -restructured solver class, not complete yet -restructured solve method, not complete yet -implemented split graph algorithm -debugged incidence matrix build -also debugged flows evalution -tree traverse and pressure evalution, not working now

--- code/HE2_Solver.py
+++ code/HE2_Solver.py
@@ -3,20 +3,28 @@ import networkx as nx
 import scipy.optimize as scop
 from HE2_SpecialEdges import HE2_DummyEdge
 import HE2_Vertices as vrtxs
+import networkx.algorithms.operators.binary as bool_ops
+import scipy.sparse
+import HE2_ABC as abc
 
 
+Root = 'Root'
 class HE2_Solver():
-
     def __init__(self, schema):
         self.schema = schema
+        self.graph = None
 
     def solve(self):
-        self.add_root_to_graph()
-        self.Q = self.build_Q_vec()
-        self.span_tree, self.chordes = self.split_graph()
-        self.tiers = self.build_tiers(self.span_tree)
-        A_tree = nx.incidence_matrix(self.span_tree)
-        self.A_inv = np.linalg.inv(A_tree)
+        self.graph = self.add_root_to_graph()
+        self.span_tree, self.chordes = self.split_graph(self.graph)
+        self.A_tree = self.buildTreeIncMatrix()
+        self.A_inv = np.linalg.inv(self.A_tree)
+        self.Q = self.build_Q_vec(self.graph)
+        x = np.matmul(self.A_inv, self.Q)
+        self.tree_x = dict(zip(self.a_tree_edgelist, x))
+        self.evalute_pressures_by_tree()
+
+        # self.tiers = self.build_tiers(self.span_tree)
         self.A_chordes = nx.incidence_matrix(self.chordes)
 
         def target(x_chordes):
@@ -40,23 +48,85 @@ class HE2_Solver():
     def build_tiers(self, tree):
         pass
 
-    def split_graph(self):
-        return None, None
+    def split_graph(self, graph):
+        G = nx.MultiGraph(graph)
+
+        t_ = nx.minimum_spanning_tree(G)
+        self.tree_travers = list(nx.algorithms.traversal.edgebfs.edge_bfs(t_, Root))
+        te_ = set(t_.edges())
+
+        c_ = bool_ops.difference(G, t_)
+        ce_ = set(c_.edges())
+
+        assert len(te_ & ce_) == 0
+        T = nx.MultiDiGraph([e for e in G.edges() if e in te_ or reversed(e) in te_])
+        C = nx.MultiDiGraph([e for e in G.edges() if e in ce_ or reversed(e) in ce_])
+
+        return T, C
 
     def add_root_to_graph(self):
         G = nx.MultiDiGraph(self.schema)
-        G.add_node('Root', obj=None)
+        G.add_node(Root, obj=None)
         for n in G.nodes:
             obj = G.nodes[n]['obj']
             if isinstance(obj, vrtxs.HE2_Boundary_Vertex) and obj.kind == 'P':
                 new_obj = vrtxs.HE2_ABC_GraphVertex()
                 G.nodes[n]['obj'] = new_obj
-                G.add_edge('Root', n, obj=HE2_DummyEdge(obj.value))
+                G.add_edge(Root, n, obj=HE2_DummyEdge(obj.value))
+        return G
 
 
+    def build_Q_vec(self, G):
+        n = len(G.nodes)
+        q_vec = np.zeros(n-1)
+        for i, node in enumerate(G.nodes):
+            obj = G.nodes[node]['obj']
+            if isinstance(obj, vrtxs.HE2_Boundary_Vertex):
+                assert obj.kind == 'Q'
+                q_vec[i] = obj.value if obj.is_source else -obj.value
+        rez = q_vec.reshape((n-1, 1))
+        return rez
 
-    def build_Q_vec(self):
-        pass
+    def buildTreeIncMatrix(self):
+        # G = self.span_tree
+        # for n in G.nodes:
+        #     print(n)
+        # for idx in G.edges:
+        #     print(idx)
+        #
+        # print('-'*80)
+        # G = self.graph
+        # for n in G.nodes:
+        #     print(n)
+        # for idx in G.edges:
+        #     print(idx)
+
+        nodelist = list(self.graph.nodes)
+        assert nodelist[-1] == Root
+        te_ = set(self.span_tree.edges())
+        edgelist =[]
+        for e in self.graph.edges():
+            er = tuple(reversed(e))
+            if e in te_ or er in te_:
+                edgelist += [e]
+
+        A_full = -1 * nx.incidence_matrix(self.span_tree, nodelist=nodelist, edgelist=edgelist, oriented=True).toarray()
+        A_truncated = A_full[:-1]
+        self.a_tree_edgelist = edgelist
+        return A_truncated
+
+    def evalute_pressures_by_tree(self):
+        pt = dict()
+        pt[Root] = (0, 20) #TODO: get initial T from some source
+        for u,v,k in self.tree_travers:
+            obj = self.graph[u][v][k]['obj']
+            assert isinstance(obj, abc.HE2_ABC_GraphEdge)
+            p_u, t_u = pt[u]
+            x = self.tree_x[(u,v,k)]
+            p_v, t_v = obj.perform_calc(p_u, t_u, x)
+            pt[v] = (p_v, t_v)
+        self.pt = pt
+
 
     def evalute_tier_pressure_drop(self, edges, p, x, fluids):
         pass
