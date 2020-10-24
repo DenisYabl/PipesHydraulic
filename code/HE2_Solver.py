31e2de093c436e384ac1993873821fcba7d1fcae
av-maltsev <amaltsev@isgneuro.com>
2020-10-24 15:02:01 +0300

    -some debug output -the best optimization method was found

--- code/HE2_Solver.py
+++ code/HE2_Solver.py
@@ -17,6 +17,7 @@ class HE2_Solver():
         self.graph = None
 
     def solve(self):
+        op_result = None
         self.graph = self.add_root_to_graph()
         self.N = len(self.graph)-1
         self.span_tree, self.chordes = self.split_graph(self.graph)
@@ -41,7 +42,15 @@ class HE2_Solver():
 
         if self.C:
             x0 = np.zeros(self.C)
-            op_result = scop.minimize(target, x0, method='Nelder-Mead')
+            # Newton-CG, dogleg, trust-ncg, trust-krylov, trust-exact не хочут, Jacobian is required
+            # 'Nelder-Mead', 'CG', L-BFGS-B, TNC, COBYLA, SLSQP - плохо сходятся, fun > 2
+            # 'BFGS' при низком tol плохо сходится, при тол < 1e-5 - норм, но долго, nfev 472
+            # Powell сходится, 350 nfev при tol 1e-6, 550 при tol 1e-3
+            # 'trust-constr' сходится, default tol, 540 nfev
+
+            op_result = scop.minimize(target, x0, method='Powell')
+            print(op_result)
+            target(op_result.x)
             self.chord_x = dict(zip(self.chordes.edges(), op_result.x))
             # TODO Вот здесь надо забирать давления по ключу op_result.x из промежуточных результатов, когду они будут сохраняться
             # А пока может быть так что давления от одной итерации, а потоки от другой
@@ -49,7 +58,7 @@ class HE2_Solver():
             target(None)
 
         self.attach_results_to_schema()
-        return
+        return op_result
 
     def build_tiers(self, tree):
         pass
@@ -201,6 +210,7 @@ class HE2_Solver():
             t_u = u_obj.result['T_C']
             p_v = v_obj.result['P_bar']
             t_v = v_obj.result['T_C']
+            print(u, v, f'{x:.2f}', f'{p_u:.2f}', f'{p_v:.2f}', edge_obj)
             p, t = edge_obj.perform_calc_forward(p_u, t_u, x)
             np.testing.assert_almost_equal(p, p_v)
             np.testing.assert_almost_equal(t, t_v)
