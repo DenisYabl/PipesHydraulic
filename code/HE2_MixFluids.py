2c0366f2dea42390bb66fd4ba77e16b2ad0bc4f9
av-maltsev <amaltsev@isgneuro.com>
2020-11-13 17:35:56 +0300

    -linal part of fluid mixation is almost done

--- code/HE2_MixFluids.py
+++ code/HE2_MixFluids.py
@@ -6,39 +6,76 @@ from HE2_Solver import HE2_Solver
 
 
 def evalute_network_fluids_wo_root(_G, x_dict):
+    assert not (Root in _G.nodes)
+
     for u, v in _G.edges:
         assert x_dict[(u, v)] >= 0
     G = nx.DiGraph(_G)
     for (u, v) in x_dict:
         if x_dict[(u, v)] == 0:
             G.remove_edge(u, v)
-    for n in _G.nodes:
-        if len(G[n])==0:
-            G.remove_node(n)
-
-    assert not (Root in G.nodes)
+    for node in _G.nodes:
+        if len(G[node]) == 0:
+            G.remove_node(node)
 
     nodes = list(G.nodes)
     edges = list(G.edges)
+    EN = len(edges)
+    N = len(nodes)
+
     x = np.array([x_dict[e] for e in edges])
     A = -1 * nx.incidence_matrix(G, nodelist=nodes, edgelist=edges, oriented=True).toarray()
     Q = np.matmul(A, x)
-    variables = [i for i, e in enumerate(edges) if x_dict[e] != 0]
-    EN = len(variables)
-    variables += [i for i, n in enumerate(nodes) if Q[i] < 0]
-    N = len(variables)
-    QN = N - EN
-    mx = np.zeros((N, N))
+    var_idx = {e:i for i, e in enumerate(edges)}
+    sinks = [n for i, n in enumerate(nodes) if Q[i] < 0]
+    var_idx.update({n:i+EN for i, n in enumerate(sinks)})
+    M = len(var_idx)
+    mx1 = np.zeros((N, M)) # the first partition of matrix is for 1stCL
+    mx2 = np.zeros((M-N, M)) # the second partition is to dictate condition: all fluids leaving one node have to be the same
+    i2 = 0
+    for i, node in enumerate(nodes):
+        these_vars_are_the_same = []
+        if Q[i] < 0: # n is sink, so there is an unknown (equation variable) for outbound flow
+            j = var_idx[node] # get the unknown index in matrix
+            mx1[i, j] = Q[i]
+            these_vars_are_the_same += [j] # remember the unknown for a while
+
+        for u, v in G.in_edges(node):  # inlet flows
+            j = var_idx[(u, v)]
+            mx1[i, j] = x_dict[(u, v)]
+
+        for u, v in G.out_edges(node): # all the outlet flows are the same
+            j = var_idx[(u, v)]
+            mx1[i, j] = -x_dict[(u, v)]
+            these_vars_are_the_same += [j] # so we have to remmeber this unknown too
+
+        if Q[i] > 0:
+            mx1[i] /= Q[i] # cause we need only ones and zeros on the right side
+
+        if len(these_vars_are_the_same) > 1: # if there are more than one remembered outlet flow
+            for fl1, fl2 in zip(these_vars_are_the_same[1:], these_vars_are_the_same[:-1]):
+                mx2[i2, fl1] = 1  # we have to right an equation to matrix, to describe that these outlet fluids are equal
+                mx2[i2, fl2] = -1
+                i2 += 1
+    mx = np.append(mx1, mx2, axis=0)
+    assert mx.shape == (M, M)
+
+    mx_inv = np.linalg.inv(mx)
+    rez_mx = np.zeros((M, N))
+    srcs = []
+    S = 0
     for i, n in enumerate(nodes):
-        if Q[i] < 0: # n is sink
-            j = get_variable_idx(n)
-            mx[i, j] = Q[i]
-        for u, v in G.in_edges(n):
-            j = get_variable_idx(u, v)
-            mx[i, j] = x_dict[(u, v)]
-        for u, v in G.out_edges(n):
-            j = get_variable_idx(u, v)
-            mx[i, j] = -x_dict[(u, v)]
+        if Q[i] > 0:
+            rez_mx[:, S] = mx_inv[:,i]
+            srcs += [n]
+            S += 1
+
+    rez = {}
+    for k, i in var_idx.items():
+        rez[k] = rez_mx[i, :S]
+        np.testing.assert_almost_equal(abs(sum(rez[k])), 1)
+    return rez, srcs
+
 
 
 if __name__ == '__main__':
@@ -56,5 +93,6 @@ if __name__ == '__main__':
         else:
             x_dict[(u, v)] = x
             G.add_edge(u, v)
-    evalute_network_fluids_wo_root(G, x_dict)
+    rez = evalute_network_fluids_wo_root(G, x_dict)
+    print(rez)
 
