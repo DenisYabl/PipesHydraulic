74af8c7b3d3a7a515eb2aa7309ad08065be9c715
av-maltsev <amaltsev@isgneuro.com>
2021-05-07 14:11:44 +0300

    -threshold is too high -we have to validate solution

--- code/DFOperations/calculate_DF.py
+++ code/DFOperations/calculate_DF.py
@@ -1,5 +1,6 @@
 from Solver.HE2_Solver import HE2_Solver
 from Tools.HE2_schema_maker import make_oilpipe_schema_from_OT_dataset
+from Tools.HE2_tools import check_solution
 import logging
 import sys
 
@@ -8,7 +9,9 @@ def calculate_DF(dataframe):
 
     G, calc_df = make_oilpipe_schema_from_OT_dataset(dataframe)
     solver = HE2_Solver(G)
-    solver.solve()
+    solver.solve(threshold = 0.2)
+    validity = check_solution(G)
+    print(validity)
     modified_dataframe = dataframe.copy()
     if solver.op_result.success == True:
         for n in G.nodes:
