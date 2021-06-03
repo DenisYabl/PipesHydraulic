eebb1dda6c9ef56baadcd4d4459c659bdeed3a59
av-maltsev <amaltsev@isgneuro.com>
2021-06-03 14:20:40 +0300

    -print solution

--- code/DFOperations/calculate_DF.py
+++ code/DFOperations/calculate_DF.py
@@ -1,6 +1,6 @@
 from Solver.HE2_Solver import HE2_Solver
 from Tools.HE2_schema_maker import make_oilpipe_schema_from_OT_dataset
-from Tools.HE2_tools import check_solution
+from Tools.HE2_tools import check_solution, print_solution
 import logging
 import sys
 
@@ -13,12 +13,12 @@ def calculate_DF(dataframe):
     modified_dataframe = dataframe.copy()
     if solver.op_result.success == True:
         validity = check_solution(G)
-        print(validity)
 
         for n in G.nodes:
             calc_df.loc[calc_df["node_id_start"] == n, "startP"] = G.nodes[n]["obj"].result["P_bar"]
             calc_df.loc[calc_df["node_id_start"] == n, "startT"] = G.nodes[n]["obj"].result["T_C"]
             calc_df.loc[calc_df["node_id_end"] == n, "endP"] = G.nodes[n]["obj"].result["P_bar"]
             calc_df.loc[calc_df["node_id_end"] == n, "endT"] = G.nodes[n]["obj"].result["T_C"]
+    print_solution(G)
 
     return calc_df
