6c7b1fa5b3a5bd5ce49a15a9127e6a52e1209dab
av-maltsev <amaltsev@isgneuro.com>
2021-06-06 08:37:25 +0300

    -mass flow, watercut, density and velocity to results

--- code/DFOperations/calculate_DF.py
+++ code/DFOperations/calculate_DF.py
@@ -13,12 +13,29 @@ def calculate_DF(dataframe):
     modified_dataframe = dataframe.copy()
     if solver.op_result.success == True:
         validity = check_solution(G)
+#        print(validity)
+#        print_solution(G)
 
         for n in G.nodes:
             calc_df.loc[calc_df["node_id_start"] == n, "startP"] = G.nodes[n]["obj"].result["P_bar"]
             calc_df.loc[calc_df["node_id_start"] == n, "startT"] = G.nodes[n]["obj"].result["T_C"]
             calc_df.loc[calc_df["node_id_end"] == n, "endP"] = G.nodes[n]["obj"].result["P_bar"]
             calc_df.loc[calc_df["node_id_end"] == n, "endT"] = G.nodes[n]["obj"].result["T_C"]
-    print_solution(G)
+
+        calc_df['res_X_kg_sec'] = None
+        calc_df['res_watercut_percent'] = None
+        calc_df['res_liquid_density_kg_m3'] = None
+
+        for index, row in calc_df.iterrows():
+            u = row['node_id_start']
+            v = row['node_id_end']
+            obj = G[u][v]['obj']
+            calc_df.loc[index, 'X_kg_sec'] = obj.result['x']
+            calc_df.loc[index, 'res_watercut_percent'] = obj.result['WC']
+            calc_df.loc[index, 'res_liquid_density_kg_m3'] = obj.result['liquid_density']
+            Q = obj.result['x'] / obj.result['liquid_density']
+            Area = 3.1415926 * row['intD'] ** 2 / 4
+            V = Q / Area
+            calc_df.loc[index, 'velocity_m_sec'] = V
 
     return calc_df
