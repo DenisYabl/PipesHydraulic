a081bc006bedc38abb99f2f163340ee24cdd6247
av-maltsev <amaltsev@isgneuro.com>
2021-06-08 12:08:48 +0300

    -mapping between MultiDiGrpah edges and calc_df rows -It should be better to reset dataframe index, after multiply rows, damned pandas

--- code/DFshowcase.py
+++ code/DFshowcase.py
@@ -61,5 +61,5 @@ mdf.to_csv("../CommonData/mdf.csv")
 Требования к датафрейму такие же, как для calculate_DF
 """
 
-# G, df = make_oilpipe_schema_from_OT_dataset(dataset)
+# G, df, df_to_graph_edges_mapping = make_oilpipe_schema_from_OT_dataset(dataset)
 pass
